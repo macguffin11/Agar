57eb015c996d7843896a222ae2898df712a8bd44
macguffin11 <luthfi.putrapayakumbuah@gmail.com>
2019-12-16 10:16:35 +0700

    reset repository

---
+++ Assets/Scripts/Dash.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dash : Utilities
+{
+    private AudioManager audioManager;
+    private GameManager gameManager;
+    private PlayerController playerController;
+
+    private float massEject;
+    public float dashSpeed = 100.0f;
+    private float tempSpeed;
+    private float dashTime;
+    public float StartDashTime;
+
+    // Use this for initialization
+    void Start()
+    {
+        massEject = Mathf.Sqrt(35f / Mathf.PI);
+        gameManager = FindObjectOfType<GameManager>();
+        if (gameManager == null)
+        {
+            Print("No GameManager found!", "error");
+        }
+        audioManager = FindObjectOfType<AudioManager>();
+        if (audioManager == null)
+        {
+            Print("No AudioManager found!", "error");
+        }
+        playerController = FindObjectOfType<PlayerController>();
+        if (playerController == null)
+        {
+            Print("No AudioManager found!", "error");
+        }
+        tempSpeed = playerController.movementSpeed;
+    }
+
+    void FixedUpdate()
+    {
+
+        if (dashTime <= 0 || transform.localScale.x <= massEject)
+        {
+            playerController.movementSpeed = tempSpeed;
+        }
+        else
+        {
+            dashTime -= Time.deltaTime;
+            playerController.movementSpeed = dashSpeed;
+            Boost();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            StartDash();
+        }
+    }
+
+    public void StartDash()
+    {
+        dashTime = StartDashTime;
+    }
+
+    public void Boost()
+    {
+        Print("Boost", "log");
+        //Mengecil dan mengurangi point
+        float radPlayer = transform.localScale.x;
+        float diff = (Mathf.PI * radPlayer * radPlayer) - 1f;
+        radPlayer = Mathf.Sqrt(diff / Mathf.PI);
+        transform.localScale = new Vector3(radPlayer, radPlayer, 0);
+        gameManager.ChangeScore(-1);
+    }
+}
---
+++ Assets/Scripts/Managers/GameOver.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Text.RegularExpressions;
+using TMPro;
+
+public class GameOver : MonoBehaviour
+{
+	private GameManager gameManager;
+
+	public TextMeshProUGUI playerNameScore;
+	public TextMeshProUGUI messageText;
+
+	public AudioManager audioManager;
+
+
+	// Use this for initialization
+	void Start()
+	{
+		Time.timeScale = 1;
+
+		gameManager = FindObjectOfType<GameManager>();
+
+		playerNameScore.text = gameManager.inputName + " - " + gameManager.currentScore;
+		messageText.text = "GAME OVER";
+
+		LoadMainMenu(5);
+	}
+
+	IEnumerator LoadSceneDelayCo(string sceneName, float delay = 0)
+	{
+		yield return new WaitForSecondsRealtime(delay);
+		SceneManager.LoadScene(sceneName);
+	}
+
+	void Update()
+	{
+	}
+
+	void LoadMainMenu(float delay = 0)
+	{
+		StartCoroutine(LoadSceneDelayCo("Main-Menu", delay));
+	}
+}
